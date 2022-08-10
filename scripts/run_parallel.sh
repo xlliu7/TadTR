1fe9a1840e7a56d9744bcc6ff4d9203db1dc63a6
Xiaolong Liu <xlliu7@users.noreply.github.com>
2022-08-10 21:52:52 +0800

    Update run_parallel.sh

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -1,9 +1,6 @@
 # Run on two GPUs in non-distributed mode (more convenient)
-CUDA_VISIBLE_DEVICES=0,1 python -u main.py --cfg "CFG_PATH"
+CUDA_VISIBLE_DEVICES=0,1 python -u main.py --cfg "CFG_PATH" --multi_gpu
 
 # Run on two GPUs in distributed mode (more powerful)
 MASTER_PORT=29510
 CUDA_VISIBLE_DEVICES=0,1 python -u -m torch.distributed.launch --nproc_per_node=2 --master_port ${MASTER_PORT} --use_env main.py --cfg "CFG_PATH"
-
-
-
